7403454bd556a4d134d87a66249d545e611752a6
l10nel <l10ne1>
2019-06-15 18:47:32 +0800

    feat(MainActivity): Full-size photo
    with getExternalStorageDirectory()
    and add photo to gallery

--- app/src/main/java/com/lionel/camerap/MainActivity.java
+++ app/src/main/java/com/lionel/camerap/MainActivity.java
@@ -7,16 +7,26 @@ import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_FULL_SIZED = 1;
     private static final int REQUEST_IMAGE_THUMBNAIL = 2;
     private ViewDataBinding dataBinding;
+    private File currentPhotoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
 
-//        checkPermission();
+        checkPermission();
     }
 
     private void checkPermission() {
@@ -44,15 +55,44 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode != REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
             checkPermission();
         }
     }
 
     public void onFullSizeClicked(View view) {
+        try {
+            currentPhotoFile = createPhotoFile();
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (intent.resolveActivity(getPackageManager()) != null && currentPhotoFile != null) {
+                Uri photoUri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".android.fileprovider", currentPhotoFile);
+                Log.d("<>", "uri: " + photoUri);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(intent, REQUEST_IMAGE_FULL_SIZED);
+            }
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private File createPhotoFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String photoFileName = "CameraP_" + timeStamp + ".jpg";
+
+        File storageDir = new File(Environment.getExternalStorageDirectory(), "cameraP_pic");   // the child name have to be same with file_paths 's path
+        if (!storageDir.exists()) {
+            storageDir.mkdir();
+        }
+        Log.d("<>", storageDir.getAbsolutePath());
+
+        File photoFile = new File(storageDir, photoFileName);
+        if (!photoFile.exists()) {
+            photoFile.createNewFile();
+        }
+        Log.d("<>", photoFile.getAbsolutePath());
+
+        return photoFile;
     }
 
     public void onThumbnailClicked(View view) {
@@ -69,6 +109,22 @@ public class MainActivity extends AppCompatActivity {
             Bitmap bitmap = (Bitmap) extra.get("data");
             dataBinding.setVariable(BR.imageFile, bitmap);
         }
+
+        if (requestCode == REQUEST_IMAGE_FULL_SIZED && resultCode == RESULT_OK) {
+            makePhotoScannable();
+
+            Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoFile.getAbsolutePath());
+            if (bitmap != null) {
+                dataBinding.setVariable(BR.imageFile, bitmap);
+            }
+        }
+    }
+
+    private void makePhotoScannable() {
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri contentUri = Uri.fromFile(currentPhotoFile);
+        intent.setData(contentUri);
+        this.sendBroadcast(intent);
     }
 
 
